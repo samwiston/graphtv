b655f56f26943fd868eb590317c24549f2fe5da1
samwiston <samwiston8@gmail.com>
2019-03-21 15:35:32 -0400

    Test Case

---
+++ src.js
@@ -0,0 +1,14 @@
+/*function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return result;
+  }
+
+loadFile("https://github.com/samwiston/graphtv/blob/master/testFile.json")*/
+
+document.getElementById("Title").innerHTML = "Something";
